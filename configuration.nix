b76ccb305fc408ad5ed069e4701baad84ad848ad
Castrozan <castro.lucas290@gmail.com>
2024-07-13 16:19:13 -0300

    init

---
+++ configuration.nix
@@ -0,0 +1,192 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Set nvidia driver
+  #services.xserver.videoDrivers = ["nvidia"];
+  
+  # Enable opengl
+  #hardware.opengl = {
+    #enable = true;
+    #driSupport = true;
+    #driSupport32Bit = true;
+  #};
+
+  # Configure to use only the dedicated GPU
+  #hardware.nvidia.prime = {
+    #sync.enable = true;
+    # integrated
+    #amdgpuBusId = "PCI:5:0:0";
+    # dedicated
+    #nvidiaBusId = "PCI:1:0:0";
+  #};
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "br";
+    xkbVariant = "nodeadkeys";
+  };
+
+  # Configure console keymap
+  console.keyMap = "br-abnt2";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.nix = {
+    isNormalUser = true;
+    description = "nix";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Enable some experimental features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    # Utils begin
+    vim
+    git
+    tmux
+    wget
+    fzf
+    jq
+    wireguard-go
+    linuxKernel.packages.linux_5_4.wireguard
+    wireguard-tools
+    ffmpeg
+    gh
+    # Utils end
+
+    # Apps begin
+    vscode
+    obsidian
+    brave
+    # Set opera to use its codecs. This enables it to display videos
+    (opera.override { 
+      proprietaryCodecs = true; 
+    })
+    steam
+    # NordVpn Wireguard client
+      # Should install manually rn from github.com/phirecc/wgnord
+    wgnord
+    # Discord and vesktop to enable screensharing on wayland
+    discord
+    vesktop
+    # Config to enable OpenAsar / Vencord
+    (discord.override {
+      withOpenASAR = true;
+      # Vencord for costumization
+      withVencord = true;
+    })
+    # Apps end
+  ];
+
+  services.xserver.wacom.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
