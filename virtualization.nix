4f5922d7c424a2f0c39255196d2ad98088e38618
Castrozan <castro.lucas290@gmail.com>
2024-08-10 19:25:48 -0300

    fix: docker config

--- virtualization.nix
+++ virtualization.nix
@@ -1,23 +1,14 @@
 { pkgs, ... }:
 {
-  ###################################################################################
-  #
-  #  Virtualisation - Libvirt(QEMU/KVM) / Docker / LXD / WayDroid
-  #
-  ###################################################################################
-
-  # From https://github.com/ryan4yin/nix-config/tree/i3-kickstarter
-
-  # virtualisation.docker.enable = true;
-  # users.extraGroups.docker.members = [ "nix" ];
-  virtualisation.docker.rootless = {
+  # Docker configuration
+  virtualisation.docker = {
     enable = true;
-    setSocketVariable = true;
+    #setSocketVariable = true;
+    #enableNvidia = true; # for nvidia-docker
 
     # start dockerd on boot.
     # This is required for containers which are created with the `--restart=always` flag to work.
-    # enableOnBoot = true;
+    enableOnBoot = true;
   };
-
-  #virtualisation.docker.enableNvidia = true; # for nvidia-docker
+  users.extraGroups.docker.members = [ "nix" ];
 }
