027493c6b38b542ac461e66d5f48fd953dd7b6e8
Castrozan <castro.lucas290@gmail.com>
2024-08-18 17:57:02 -0300

    feat: virt-Manager

--- virtualization.nix
+++ virtualization.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   # Docker configuration
   virtualisation.docker = {
@@ -11,4 +11,38 @@
     enableOnBoot = true;
   };
   users.extraGroups.docker.members = [ "nix" ];
+
+  # Virt-Manager
+  # From https://github.com/TechsupportOnHold/Nixos-VM
+  # Enable dconf (System Management Tool)
+  programs.dconf.enable = true;
+
+  # Add user to libvirtd group
+  users.users.nix.extraGroups = [ "libvirtd" ];
+
+  # Install necessary packages
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    virt-viewer
+    spice
+    spice-gtk
+    spice-protocol
+    win-virtio
+    win-spice
+    gnome.adwaita-icon-theme
+  ];
+
+  # Manage the virtualisation services
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [ pkgs.OVMFFull.fd ];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
 }
